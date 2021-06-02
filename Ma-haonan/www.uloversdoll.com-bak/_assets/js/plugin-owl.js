7bba592d4b7ce08f5fe3e7a97bd92202057a870e
KouJun <sanseistone@users.noreply.github.com>
2021-06-02 18:26:31 +0800

    revise
    revise

---
+++ Ma-haonan/www.uloversdoll.com-bak/_assets/js/plugin-owl.js
@@ -0,0 +1,170 @@
+$(document).ready(function () {
+    $('.owl-one').owlCarousel({
+        loop: true,
+        margin: 30,
+        navText: ['<i class="fal fa-angle-left"></i>', '<i class="fal fa-angle-right"></i>'],
+        autoplay: false,
+        autoplayTimeout: 50000,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true,
+                dots: false
+            },
+            575: {
+                items: 1,
+                nav: true,
+                dots: false
+            },
+            991: {
+                items: 1,
+                nav: true,
+                dots: false
+            },
+            1199: {
+                items: 1,
+                nav: true,
+                dots: false
+            }
+        }
+    });
+    $('.owl-three').owlCarousel({
+        loop: true,
+        margin: 30,
+        navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+        autoplay: false,
+        autoplayTimeout: 50000,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2,
+                nav: true,
+                dots: false
+            },
+            575: {
+                items: 2,
+                nav: true,
+                dots: false
+            },
+            991: {
+                items: 3,
+                nav: true,
+                dots: false
+            },
+            1199: {
+                items: 3,
+                nav: true,
+                dots: false
+            }
+        }
+    });
+    $('.owl-four').owlCarousel({
+        loop: true,
+        margin: 30,
+        navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+        autoplay: false,
+        autoplayTimeout: 50000,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2,
+                nav: true,
+                dots: false
+            },
+            575: {
+                items: 3,
+                nav: true,
+                dots: false
+            },
+            991: {
+                items: 3,
+                nav: true,
+                dots: false
+            },
+            1199: {
+                items: 4,
+                nav: true,
+                dots: false
+            }
+        }
+    });
+    $('.owl-five').owlCarousel({
+        loop: true,
+        margin: 15,
+        navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+        autoplay: true,
+        autoplayTimeout: 50000,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2,
+                nav: true,
+                dots: false
+            },
+            575: {
+                items: 3,
+                nav: true,
+                dots: false
+            },
+            991: {
+                items: 4,
+                nav: true,
+                dots: false
+            },
+            1199: {
+                items: 5,
+                nav: true,
+                dots: false
+            }
+        }
+    });
+    $('.owl-six').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fal fa-angle-left"></i>', '<i class="fal fa-angle-right"></i>'],
+        autoplay: true,
+        autoplayTimeout: 50000,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            575: {
+                items: 3
+            },
+            991: {
+                items: 4
+            },
+            1199: {
+                items: 6
+            }
+        }
+    });
+    $('.owl-seven').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fal fa-angle-left"></i>', '<i class="fal fa-angle-right"></i>'],
+        autoplay: true,
+        autoplayTimeout: 50000,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            575: {
+                items: 3
+            },
+            991: {
+                items: 5
+            },
+            1199: {
+                items: 7
+            }
+        }
+    });
+});
--- Ma-haonan/www.uloversdoll.com-bak/_assets/js/plugin.js
+++ Ma-haonan/www.uloversdoll.com-bak/_assets/js/plugin.js
@@ -6,11 +6,11 @@ $(document).ready(function () {
     // });
 
     $(document).on('touchstart', '.navbar-toggler', function (event) {
-        $('.navbar-box').fadeIn(0).find('.navbar').addClass('active');
+        $('.navbar-box').fadeIn(0).find('.navbar-inner').addClass('active');
     });
     $(document).on('touchstart', '.navbar-box', function (e) {
-        if ($(e.target).closest('.navbar').length === 0) {
-            $('.navbar-box .navbar').removeClass('active');
+        if ($(e.target).closest('.navbar-inner').length === 0) {
+            $('.navbar-box .navbar-inner').removeClass('active');
             setTimeout("$('.navbar-box').fadeOut(50)", 220);
         }
     });
