ae51f32f8220a09c37a94239111fb5a882ff6351
KouJun <sanseistone@users.noreply.github.com>
2021-06-16 17:38:11 +0800

    revise
    revise

--- Peng-yintong/www.oksexdoll.com/_assets/js/plugin.js
+++ Peng-yintong/www.oksexdoll.com/_assets/js/plugin.js
@@ -1,23 +1,6 @@
-$(".write-reviews-box .click-ask").click(function() {
-    if($(".ask-question").hasClass("hide")) {
-        $(".ask-question").removeClass("hide");
-    } else {
-        $(".ask-question").addClass("hide");
-    }
-});
-
-$(".write-reviews-box .click-write").click(function() {
-    if($(".write-review").hasClass("hide")) {
-        $(".write-review").removeClass("hide");
-    } else {
-        $(".write-review").addClass("hide");
-    }
-});
-
-$(".form-user-box .click-link").click(function() {
-    if($(this).parent().next().hasClass("hide")) {
-        $(this).parent().next().removeClass("hide");
-    } else {
-        $(this).parent().next().addClass("hide");
-    }
+$(document).ready(function () {
+    // if (!window.sessionStorage.getItem('storge')) {
+    //     $('#are-you-ready').modal('show');
+    //     window.sessionStorage.setItem('storge', 'true')
+    // }
 });
