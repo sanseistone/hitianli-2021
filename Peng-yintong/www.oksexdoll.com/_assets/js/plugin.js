723c57396d4b92b8f48e51a592272a9e248d1a6f
KouJun <sanseistone@users.noreply.github.com>
2021-06-17 12:57:26 +0800

    revise
    revise

--- Peng-yintong/www.oksexdoll.com/_assets/js/plugin.js
+++ Peng-yintong/www.oksexdoll.com/_assets/js/plugin.js
@@ -1,6 +1,45 @@
 $(document).ready(function () {
-    // if (!window.sessionStorage.getItem('storge')) {
-    //     $('#are-you-ready').modal('show');
-    //     window.sessionStorage.setItem('storge', 'true')
-    // }
+    // $('.nav-main li.dropdown').mouseover(function () {
+    //     $(this).children(".dropdown-menu").addClass('show');
+    // }).mouseout(function () {
+    //     $(this).children(".dropdown-menu").removeClass('show');
+    // });
+
+    $(document).ready(function () {
+        if (screen.width > 992) {
+            $(window).on('scroll', function () {
+                if ($(window).scrollTop() >= 220) {
+                    $(".navbar.nav-main").removeClass("justify-center");
+                    $(".navbar.nav-main").addClass("justify-between");
+                    $("header .logo").css("display", "none");
+                    $(".navbar-box .logo-box").fadeIn(500);
+                } else {
+                    $(".navbar.nav-main").removeClass("justify-between");
+                    $(".navbar.nav-main").addClass("justify-center");
+                    $("header .logo").css("display", "block");
+                    $(".navbar-box .logo-box").fadeOut(50);
+                }
+            });
+        }
+
+        if (screen.width <= 992) {
+            $(window).on('scroll', function () {
+                if ($(window).scrollTop() >= 220) {
+                    $("header .row > div:nth-of-type(4)").css("display", "none");
+                } else {
+                    $("header .row > div:nth-of-type(4)").css("display", "block");
+                }
+            });
+        }
+    });
+
+    $(document).on('touchstart', '.navbar-toggler', function (event) {
+        $('.navbar-box').fadeIn(0).find('.navbar').addClass('active');
+    });
+    $(document).on('touchstart', '.navbar-box', function (e) {
+        if ($(e.target).closest('.navbar').length === 0) {
+            $('.navbar-box .navbar').removeClass('active');
+            setTimeout("$('.navbar-box').fadeOut(50)", 220);
+        }
+    });
 });
