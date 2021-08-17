2a933b8fad1bfb8ffd7c5fe5fe247576bf29c6ec
sanseistone <mr.koujun@gmail.com>
2021-08-17 18:11:24 +0800

    revise
    revise

--- Peng-yintong/www.rabudull.com/assets/js/plugin.js
+++ Peng-yintong/www.rabudull.com/assets/js/plugin.js
@@ -1,8 +1,20 @@
 $(document).ready(function () {
-    $('.nav-main li.dropdown').mouseover(function () {
-        $(this).children(".dropdown-menu").addClass('show');
-    }).mouseout(function () {
-        $(this).children(".dropdown-menu").removeClass('show');
+    // $('.nav-main li.dropdown').mouseover(function () {
+    //     $(this).children(".dropdown-menu").addClass('show');
+    // }).mouseout(function () {
+    //     $(this).children(".dropdown-menu").removeClass('show');
+    // });
+
+    window.onscroll = function () {
+        var navigation_box = $(document).scrollTop();
+        if (navigation_box > 200) {
+            $(".navigation-box .back-to-top").css("display", "block");
+        } else {
+            $(".navigation-box .back-to-top").css("display", "none");
+        }
+    }
+    $(".back-to-top").click(function () {
+        $('body,html').animate({scrollTop: 0}, 500);
     });
 
     $(function() {
