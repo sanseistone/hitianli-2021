f3e88afecedd2e0f37759db0642b25aea6d566bb
sanseistone <mr.koujun@gmail.com>
2021-09-03 16:41:12 +0800

    revise
    revise

--- bootstrap-4/Zhang-xiaojie/www.jammer-buy.com/_assets/js/plugin.js
+++ bootstrap-4/Zhang-xiaojie/www.jammer-buy.com/_assets/js/plugin.js
@@ -1,23 +1,3 @@
-$(".write-reviews-box .click-ask").click(function() {
-    if($(".ask-question").hasClass("hide")) {
-        $(".ask-question").removeClass("hide");
-    } else {
-        $(".ask-question").addClass("hide");
-    }
-});
+$(document).ready(function () {
 
-$(".write-reviews-box .click-write").click(function() {
-    if($(".write-review").hasClass("hide")) {
-        $(".write-review").removeClass("hide");
-    } else {
-        $(".write-review").addClass("hide");
-    }
-});
-
-$(".form-user-box .click-link").click(function() {
-    if($(this).parent().next().hasClass("hide")) {
-        $(this).parent().next().removeClass("hide");
-    } else {
-        $(this).parent().next().addClass("hide");
-    }
 });
