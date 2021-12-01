ae73b94a2d8990c101369654f47e04b41b32fcea
sanseistone <mr.koujun@gmail.com>
2021-12-01 18:29:13 +0800

    revise
    revise

--- bootstrap-5/Xiang-yubin/www.beerdoll.com/_assets/js/plugin.js
+++ bootstrap-5/Xiang-yubin/www.beerdoll.com/_assets/js/plugin.js
@@ -1,31 +1,258 @@
 $(document).ready(function () {
-    $('.nav-main li.dropdown').mouseover(function () {
-        $(this).children(".dropdown-menu").addClass('show');
-    }).mouseout(function () {
-        $(this).children(".dropdown-menu").removeClass('show');
+    //back-to-top
+    window.onscroll = function () {
+        var navigation_box = $(document).scrollTop();
+        if (navigation_box > 200) {
+            $(".navigation-box .back-to-top").css("display", "block");
+        } else {
+            $(".navigation-box .back-to-top").css("display", "none");
+        }
+    }
+    $(".back-to-top").click(function () {
+        $('body,html').animate({scrollTop: 0}, 500);
     });
 
-    $('.aside ul li').has('ul').append('<span class="touch-button"><i>open</i></span>');
-    $('.touch-button').click(function () {
-        $(this).prev().slideToggle(200);
-        $(this).toggleClass('active');
-        $(this).parent().toggleClass('active');
+    //lazyload
+    $(function () {
+        $("img.lazy").lazyload({
+            effect: "fadeIn"
+        });
     });
 
-    $('.btn-plus').click(function () {
-        $('.qty-input').val(Number($('.qty-input').val()) + 1);
+    //readmore-box
+    $(function () {
+        $('.readmore-box').readmore({
+            speed: 75,
+            collapsedHeight: 45,
+            moreLink: '<div class="mt-3"><button class="d-inline-flex btn btn-sm btn-dark" type="button">More <img src="_assets/img/svg/chevron-down-white.svg" alt=""></button></div>',
+            lessLink: '<div class="mt-3"><button class="d-inline-flex btn btn-sm btn-danger" type="button">Less <img src="_assets/img/svg/chevron-up-white.svg" alt=""></button></div>'
+        });
     });
-    $('.btn-minus').click(function () {
-        var value = Number($('.qty-input').val()) - 1;
-        if (value > 0) {
-            $('.qty-input').val(value);
+
+    $('#customize-check').click(function(){
+        $('#customize-text').toggle();
+    });
+});
+
+$(document).ready(function () {
+    var containerEl = document.querySelector('.main-content');
+    var checkboxGroup = document.querySelector('.checkbox-group');
+    var checkboxes = checkboxGroup.querySelectorAll('input[type="checkbox"]');
+
+    var mixer = mixitup(containerEl);
+
+    checkboxGroup.addEventListener('change', function () {
+        var selectors = [];
+
+        for (var i = 0; i < checkboxes.length; i++) {
+            var checkbox = checkboxes[i];
+
+            if (checkbox.checked) selectors.push(checkbox.value);
+        }
+
+        var selectorString = selectors.length > 0 ?
+            selectors.join(',') :
+            'all';
+
+        mixer.filter(selectorString);
+    });
+});
+
+$(document).ready(function () {
+    //countdown
+    $('.countdown-box').downCount({
+        date: '12/12/2030 00:00:00',
+        offset: +9
+    }, function () {
+        alert('カウントダウン!');
+    });
+    $('.countdown-special').downCount({
+        date: '12/12/2030 00:00:00',
+        offset: +9
+    }, function () {
+        alert('カウントダウン!');
+    });
+});
+
+$(document).ready(function () {
+    //owlCarousel
+    $('.owl-ten').owlCarousel({
+        loop: true,
+        margin: 20,
+        navText: ['<img src="_assets/img/svg/caret-left-fill.svg" alt="">', '<img src="_assets/img/svg/caret-right-fill.svg" alt="">'],
+        autoplay: true,
+        autoplayTimeout: 50000,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 3,
+                nav: true,
+                dots: false
+            },
+            575: {
+                items: 6,
+                nav: true,
+                dots: false
+            },
+            991: {
+                items: 8,
+                nav: true,
+                dots: false
+            },
+            1199: {
+                items: 10,
+                nav: true,
+                dots: false
+            }
         }
     });
+    $('.owl-eight').owlCarousel({
+        loop: true,
+        margin: 20,
+        navText: ['<img src="_assets/img/svg/caret-left-fill.svg" alt="">', '<img src="_assets/img/svg/caret-right-fill.svg" alt="">'],
+        autoplay: true,
+        autoplayTimeout: 50000,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 3,
+                nav: true,
+                dots: false
+            },
+            575: {
+                items: 4,
+                nav: true,
+                dots: false
+            },
+            991: {
+                items: 6,
+                nav: true,
+                dots: false
+            },
+            1199: {
+                items: 8,
+                nav: true,
+                dots: false
+            }
+        }
+    });
+    $('.owl-six').owlCarousel({
+        loop: true,
+        margin: 20,
+        navText: ['<img src="_assets/img/svg/caret-left-fill.svg" alt="">', '<img src="_assets/img/svg/caret-right-fill.svg" alt="">'],
+        autoplay: true,
+        autoplayTimeout: 50000,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2,
+                nav: true,
+                dots: false
+            },
+            575: {
+                items: 3,
+                nav: true,
+                dots: false
+            },
+            991: {
+                items: 5,
+                nav: true,
+                dots: false
+            },
+            1199: {
+                items: 6,
+                nav: true,
+                dots: false
+            }
+        }
+    });
+    $('.owl-five').owlCarousel({
+        loop: true,
+        margin: 20,
+        navText: ['<img src="_assets/img/svg/caret-left-fill.svg" alt="">', '<img src="_assets/img/svg/caret-right-fill.svg" alt="">'],
+        autoplay: true,
+        autoplayTimeout: 50000,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2,
+                nav: true,
+                dots: false
+            },
+            575: {
+                items: 3,
+                nav: true,
+                dots: false
+            },
+            991: {
+                items: 4,
+                nav: true,
+                dots: false
+            },
+            1199: {
+                items: 5,
+                nav: true,
+                dots: false
+            }
+        }
+    });
+    $('.owl-four').owlCarousel({
+        loop: true,
+        margin: 20,
+        navText: ['<img src="_assets/img/svg/caret-left-fill.svg" alt="">', '<img src="_assets/img/svg/caret-right-fill.svg" alt="">'],
+        autoplay: true,
+        autoplayTimeout: 50000,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2,
+                margin: 5,
+                nav: true,
+                dots: false
+            },
+            575: {
+                items: 3,
+                margin: 10,
+                nav: true,
+                dots: false
+            },
+            991: {
+                items: 4,
+                margin: 15,
+                nav: true,
+                dots: false
+            },
+            1199: {
+                items: 4,
+                margin: 10,
+                nav: true,
+                dots: false
+            }
+        }
+    });
+});
 
-    $('.readmore-box').readmore({
-        speed: 75,
-        collapsedHeight: 170,
-        moreLink: '<p class="mt-3"><span class="inline-block px-3 py-2 bd bd-color3 pointer">More <i class="fal fa-angle-down"></i></span></p>',
-        lessLink: '<p class="mt-3"><span class="inline-block px-3 py-2 bd bd-color3 pointer">Close <i class="fal fa-angle-up"></i></span></p>'
+$(document).ready(function () {
+    //slider
+    $('.slider-main').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        dots: true,
+        asNavFor: '.slider-thumbs'
+    });
+    $('.slider-thumbs').slick({
+        asNavFor: '.slider-main',
+        // dots: true,
+        vertical: true,
+        verticalSwiping: true,
+        // centerMode: true,
+        centerPadding: '0',
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: true,
+        speed: 600,
+        focusOnSelect: true,
     });
 });
