ef50651642d62ec8dbff5bd2008d69e5ab227963
sanseistone <mr.koujun@gmail.com>
2021-12-23 18:43:50 +0800

    revise
    revise

--- bootstrap-5/User/Shopping-cart/_assets/js/plugin.js
+++ bootstrap-5/User/Shopping-cart/_assets/js/plugin.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
+    //back-to-top
+    window.onscroll = function () {
+        var navigation_box = $(document).scrollTop();
+        if (navigation_box > 200) {
+            $(".navigation-box .back-to-top").css("display", "block");
+        } else {
+            $(".navigation-box .back-to-top").css("display", "none");
+        }
+    }
+    $(".back-to-top").click(function () {
+        $('body,html').animate({scrollTop: 0}, 500);
+    });
 
+    //screen
+    if (screen.width < 768) {
+        $('.footer-menu .heading').click(function () {
+            $(this).next('.list').slideToggle();
+        });
+    }
 });
