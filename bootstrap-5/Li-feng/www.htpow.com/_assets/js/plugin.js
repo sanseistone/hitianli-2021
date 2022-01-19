dd165644e2484d3edff431280665bbd151f11ee7
KouJun <sanseistone@users.noreply.github.com>
2022-01-19 18:07:53 +0800

    revise
    revise

--- bootstrap-5/Li-feng/www.htpow.com/_assets/js/plugin.js
+++ bootstrap-5/Li-feng/www.htpow.com/_assets/js/plugin.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
     $(function () {
         $('.readmore-box').readmore({
             speed: 75,
-            collapsedHeight: 45,
+            collapsedHeight: 145,
             moreLink: '<div class="mt-3"><button class="d-inline-flex btn btn-sm btn-dark" type="button">More <img src="_assets/img/svg/chevron-down-white.svg" alt=""></button></div>',
             lessLink: '<div class="mt-3"><button class="d-inline-flex btn btn-sm btn-danger" type="button">Less <img src="_assets/img/svg/chevron-up-white.svg" alt=""></button></div>'
         });
@@ -63,7 +63,7 @@ $(document).ready(function () {
         date: '12/12/2030 00:00:00',
         offset: +9
     }, function () {
-        alert('カウントダウン!');
+        alert('down');
     });
 });
 
@@ -242,7 +242,7 @@ $(document).ready(function () {
         verticalSwiping: true,
         // centerMode: true,
         centerPadding: '0',
-        slidesToShow: 5,
+        slidesToShow: 4,
         slidesToScroll: 1,
         arrows: true,
         speed: 600,
